51dde918fe56412b50069d468220aa9be03f30f3
amyvrm <amyvrm@gmail.com>
2021-10-10 19:02:59 +0530

    Updated code

--- src/add.py
+++ src/add.py
@@ -6,4 +6,5 @@ def add(num1, num2):
 if __name__ == "__main__":
     num1 = int(input("Enter num1:"))
     num2 = int(input("Enter num2:"))
-    print("Sum of two number is: {}".format(add(num1,num2)))
+    sum = add(num1,num2)
+    print("Sum of {} and {} is {}".format(num1, num2, sum))
