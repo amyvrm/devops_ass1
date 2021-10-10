82211b4774d0cba8a8d997405363ec73e61cc0bd
amyvrm <amyvrm@gmail.com>
2021-10-10 19:23:43 +0530

    That fixed #4

--- src/add.py
+++ src/add.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 
-def add(num1, num2):
-    return num1 + num2
+def add(num_list):
+    sum = 0
+    for num in num_list:
+        sum += num
+    return sum
 
 if __name__ == "__main__":
     num1 = int(input("Enter num1:"))
     num2 = int(input("Enter num2:"))
-    sum = add(num1,num2)
-    print("Sum of {} and {} is {}".format(num1, num2, sum))
+    num3 = int(input("Enter num3:"))
+    sum_list = [num1, num2, num3]
+    sum = add(num_list)
+    print("Number list: {} and Sum of list is {}".format(num_list, sum))
